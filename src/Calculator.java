5ed4ee84edf33705a591e9622a9741c5328a07e6
Neifelheyn <134166249+Neifelheyn@users.noreply.github.com>
2023-05-21 00:58:25 +0300

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class Calculator {
+    private  enum ePriority{
+        SKOBKA
+    }
+    public void Start() {
+        String formula = UserInput();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    private String UserInput(){
+        String  formula = "";
+        Scanner keyboard = new Scanner(System.in);
+        formula = keyboard.nextLine();
+
+
+        return formula;
+    }
+
+    private void Calculete(String formula_) {
+
+
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+Calculator calculator = new Calculator();
+calculator.Start();
+
+
+
+
+
+        // 10 + 8 * (8 - 9 * 6
+        // 10 + 8 * (8 - 9 * 6)
+        // 10 + 8 * (8 - 9) * 6
+        // 10 + 8 * (8) - 9 * 6
+    }
+}
