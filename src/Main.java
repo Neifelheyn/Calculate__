ea6a0c5992cdb4cf0a321852e43c6b491ddddf48
Neifelheyn <neifelheyn@gmail.com>
2023-05-21 01:04:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-Calculator calculator = new Calculator();
-calculator.Start();
-
-
-
-
-
-        // 10 + 8 * (8 - 9 * 6
-        // 10 + 8 * (8 - 9 * 6)
-        // 10 + 8 * (8 - 9) * 6
-        // 10 + 8 * (8) - 9 * 6
+	Calculator calculator = new Calculator();
+	calculator.Start();
     }
 }
